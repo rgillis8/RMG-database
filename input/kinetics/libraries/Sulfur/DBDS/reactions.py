494e3b4ee6fa5a1ba76fbdf46c410c9173c98bfc
rgillis8 <rgillis@mit.edu>
2016-11-17 14:20:12 -0500

    added an additional reaction to the library

--- input/kinetics/libraries/Sulfur/DBDS/reactions.py
+++ input/kinetics/libraries/Sulfur/DBDS/reactions.py
@@ -10,12 +10,26 @@ entry(
     index = 1,
     label = "C7H7 + C7H6S <=> C14H13S",
     degeneracy = 1,
-    kinetics = Arrhenius(A=(2096, 'cm^3/(mol*s)'),
-                         n= 2.526,
-                         Ea=(1.15, 'kcal/mol'),
+    kinetics = Arrhenius(A=(38.2, 'cm^3/(mol*s)'),
+                         n= 2.76,
+                         Ea=(4.51, 'kcal/mol'),
                          T0=(1, 'K')
                          ),
     longDesc = 
+u"""
+""",
+)
+
+entry(
+    index = 2,
+    label = "C7H7 + C7H6S <=> DBMS_rad",
+    degeneracy = 1,
+    kinetics = Arrhenius(A=(2096, 'cm^3/(mol*s)'),
+                         n= 2.526,
+                         Ea=(3.15, 'kcal/mol'),
+                         T0=(1, 'K')
+                         ),
+    longDesc =
 u"""
 """,
 )
