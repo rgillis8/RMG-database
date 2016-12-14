565cf0c8af831220a299f4af4435add6bdd5cab4
Ryan Gillis <rgillis@mit.edu>
2016-12-14 15:24:47 -0500

    put the forward rate of the intra_H_migration reactions important to the system in the DBDS library

--- input/kinetics/libraries/Sulfur/DBDS/reactions.py
+++ input/kinetics/libraries/Sulfur/DBDS/reactions.py
@@ -20,3 +20,49 @@ u"""
 Combination of GAV value and calculated value
 """,
 )
+
+entry(
+    index = 2,
+    label = "C14H13S3 <=> C14H12S3H",
+    degeneracy = 1,
+    kinetics = Arrhenius(A=(92.2, 's^-1'),
+                         n= 3.22,
+                         Ea=(13.1, 'kcal/mol'),
+                         T0=(1, 'K')
+                         ),
+    longDesc =
+u"""
+forward reaction estimated by RMG... but not thermo favored so they use the reverse reaction. Changed by Ryan Gillis.
+""",
+)
+
+entry(
+    index = 3,
+    label = "C14H13S4 <=> C14H12S4H",
+    degeneracy = 1,
+    kinetics = Arrhenius(A=(92.2, 's^-1'),
+                         n= 3.22,
+                         Ea=(13.1, 'kcal/mol'),
+                         T0=(1, 'K')
+                         ),
+    longDesc =
+u"""
+forward reaction estimated by RMG... but not thermo favored so they use the reverse reaction. Changed by Ryan Gillis.
+""",
+)
+
+entry(
+    index = 4,
+    label = "C14H13S2 <=> C14H12S2H",
+    degeneracy = 1,
+    kinetics = Arrhenius(A=(3560000, 's^-1'),
+                         n= 1.75,
+                         Ea=(25.3, 'kcal/mol'),
+                         T0=(1, 'K')
+                         ),
+    longDesc =
+u"""
+forward reaction estimated by RMG... but not thermo favored so they use the reverse reaction. Changed by Ryan Gillis.
+""",
+)
+
