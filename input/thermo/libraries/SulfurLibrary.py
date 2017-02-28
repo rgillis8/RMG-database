c3986f5f5d3a58a873e62c0d92c16fa22ffc17da
Ryan Gillis <rgillis@mit.edu>
2017-02-28 15:48:29 -0500

    Temporary taking out radical thermo calculations

--- input/thermo/libraries/SulfurLibrary.py
+++ input/thermo/libraries/SulfurLibrary.py
@@ -6193,38 +6193,38 @@ Singlet sulfur, thermo data copied from triplet sulfur, likely very incorrect.
 """,
 )
 
-entry(
-      index = 211,
-      label = "benzyl",
-      molecule =
-"""
-multiplicity 2
-1  C u0 p0 c0 {2,B} {6,B} {8,S}
-2  C u0 p0 c0 {1,B} {3,B} {9,S}
-3  C u0 p0 c0 {2,B} {4,B} {10,S}
-4  C u0 p0 c0 {3,B} {5,B} {11,S}
-5  C u0 p0 c0 {4,B} {6,B} {12,S}
-6  C u0 p0 c0 {1,B} {5,B} {7,S}
-7  C u1 p0 c0 {6,S} {13,S} {14,S}
-8  H u0 p0 c0 {1,S}
-9  H u0 p0 c0 {2,S}
-10 H u0 p0 c0 {3,S}
-11 H u0 p0 c0 {4,S}
-12 H u0 p0 c0 {5,S}
-13 H u0 p0 c0 {7,S}
-14 H u0 p0 c0 {7,S}
-""",
-    thermo = ThermoData(
-       Tdata = ([300,400,500,600,800,1000,1500],'K'),
-       Cpdata = ([25.287,33.517,40.191,45.494,53.803,59.724,67.94],'cal/(mol*K)','+|-',[1,1,1,1,1,1,1]),
-       H298 = (51.179,'kcal/mol','+|-',1),
-       S298 = (75.032,'cal/(mol*K)','+|-',1),
-    ),
-    shortDesc = u"""Ryan Gillis CBS-QB3 Calculation""",
-    longDesc =
-u"""
-""",
-)
+#entry(
+#      index = 211,
+#      label = "benzyl",
+#      molecule =
+#"""
+#multiplicity 2
+#1  C u0 p0 c0 {2,B} {6,B} {8,S}
+#2  C u0 p0 c0 {1,B} {3,B} {9,S}
+#3  C u0 p0 c0 {2,B} {4,B} {10,S}
+#4  C u0 p0 c0 {3,B} {5,B} {11,S}
+#5  C u0 p0 c0 {4,B} {6,B} {12,S}
+#6  C u0 p0 c0 {1,B} {5,B} {7,S}
+#7  C u1 p0 c0 {6,S} {13,S} {14,S}
+#8  H u0 p0 c0 {1,S}
+#9  H u0 p0 c0 {2,S}
+#10 H u0 p0 c0 {3,S}
+#11 H u0 p0 c0 {4,S}
+#12 H u0 p0 c0 {5,S}
+#13 H u0 p0 c0 {7,S}
+#14 H u0 p0 c0 {7,S}
+#""",
+#    thermo = ThermoData(
+#       Tdata = ([300,400,500,600,800,1000,1500],'K'),
+#       Cpdata = ([25.287,33.517,40.191,45.494,53.803,59.724,67.94],'cal/(mol*K)','+|-',[1,1,1,1,1,1,1]),
+#       H298 = (51.179,'kcal/mol','+|-',1),
+#       S298 = (75.032,'cal/(mol*K)','+|-',1),
+#    ),
+#    shortDesc = u"""Ryan Gillis CBS-QB3 Calculation""",
+#    longDesc =
+#u"""
+#""",
+#)
 
 entry(
     index = 213,
@@ -6274,40 +6274,40 @@ Added by Ryan Gillis for DBDS Mechanism
 """,
 )
 
-entry(
-    index = 214,
-    label = "C7H7S",
-    molecule =
-"""
-multiplicity 2
-1  C u0 p0 c0 {2,B} {6,B} {9,S}
-2  C u0 p0 c0 {1,B} {3,B} {10,S}
-3  C u0 p0 c0 {2,B} {4,B} {11,S}
-4  C u0 p0 c0 {3,B} {5,B} {12,S}
-5  C u0 p0 c0 {4,B} {6,B} {13,S}
-6  C u0 p0 c0 {1,B} {5,B} {7,S}
-7  C u0 p0 c0 {6,S} {8,S} {14,S} {15,S}
-8  S u1 p2 c0 {7,S}
-9  H u0 p0 c0 {1,S}
-10 H u0 p0 c0 {2,S}
-11 H u0 p0 c0 {3,S}
-12 H u0 p0 c0 {4,S}
-13 H u0 p0 c0 {5,S}
-14 H u0 p0 c0 {7,S}
-15 H u0 p0 c0 {7,S}
-""",
-    thermo = ThermoData(
-        Tdata = ([300,400,500,600,800,1000,1500],'K'),
-        Cpdata = ([27.762,35.837,42.653,48.292,57.095,63.293,71.750],'cal/(mol*K)','+|-',[1,1,1,1,1,1,1]),
-        H298 = (56.497,'kcal/mol','+|-',1),
-        S298 = (83.183,'cal/(mol*K)','+|-',1),
-    ),
-    shortDesc = u"""Ryan Gillis CBS-QB3""",
-    longDesc =
-u"""
-CBS-QB3 calculation for use originally in the DBDS cycle
-""",
-)
+#entry(
+#    index = 214,
+#    label = "C7H7S",
+#    molecule =
+#"""
+#multiplicity 2
+#1  C u0 p0 c0 {2,B} {6,B} {9,S}
+#2  C u0 p0 c0 {1,B} {3,B} {10,S}
+#3  C u0 p0 c0 {2,B} {4,B} {11,S}
+#4  C u0 p0 c0 {3,B} {5,B} {12,S}
+#5  C u0 p0 c0 {4,B} {6,B} {13,S}
+#6  C u0 p0 c0 {1,B} {5,B} {7,S}
+#7  C u0 p0 c0 {6,S} {8,S} {14,S} {15,S}
+#8  S u1 p2 c0 {7,S}
+#9  H u0 p0 c0 {1,S}
+#10 H u0 p0 c0 {2,S}
+#11 H u0 p0 c0 {3,S}
+#12 H u0 p0 c0 {4,S}
+#13 H u0 p0 c0 {5,S}
+#14 H u0 p0 c0 {7,S}
+#15 H u0 p0 c0 {7,S}
+#""",
+#    thermo = ThermoData(
+#        Tdata = ([300,400,500,600,800,1000,1500],'K'),
+#        Cpdata = ([27.762,35.837,42.653,48.292,57.095,63.293,71.750],'cal/(mol*K)','+|-',[1,1,1,1,1,1,1]),
+#        H298 = (56.497,'kcal/mol','+|-',1),
+#        S298 = (83.183,'cal/(mol*K)','+|-',1),
+#    ),
+#    shortDesc = u"""Ryan Gillis CBS-QB3""",
+#    longDesc =
+#u"""
+#CBS-QB3 calculation for use originally in the DBDS cycle
+#""",
+#)
 
 entry(
     index = 215,
@@ -6405,41 +6405,41 @@ CBS-QB3 calculation for use originally in the DBDS cycle
 """,
 )
 
-entry(
-    index = 217,
-    label = "c7h7s2",
-    molecule =
-"""
-multiplicity 2
-1  C u0 p0 c0 {2,B} {6,B} {10,S}
-2  C u0 p0 c0 {1,B} {3,B} {11,S}
-3  C u0 p0 c0 {2,B} {4,B} {12,S}
-4  C u0 p0 c0 {3,B} {5,B} {13,S}
-5  C u0 p0 c0 {4,B} {6,B} {14,S}
-6  C u0 p0 c0 {1,B} {5,B} {7,S}
-7  C u0 p0 c0 {6,S} {8,S} {15,S} {16,S}
-8  S u0 p2 c0 {7,S} {9,S}
-9  S u1 p2 c0 {8,S}
-10 H u0 p0 c0 {1,S}
-11 H u0 p0 c0 {2,S}
-12 H u0 p0 c0 {3,S}
-13 H u0 p0 c0 {4,S}
-14 H u0 p0 c0 {5,S}
-15 H u0 p0 c0 {7,S}
-16 H u0 p0 c0 {7,S}
-""",
-    thermo = ThermoData(
-        Tdata = ([300,400,500,600,800,1000,1500],'K'),
-        Cpdata = ([33.379,41.558,48.886,55.181,64.567,70.718,79.669],'cal/(mol*K)','+|-',[1,1,1,1,1,1,1]),
-        H298 = (43.829,'kcal/mol','+|-',1),
-        S298 = (97.491,'cal/(mol*K)','+|-',1),
-    ),
-    shortDesc = u"""Ryan Gillis CBS-QB3""",
-    longDesc =
-u"""
-CBS-QB3 calculation for use originally in the DBDS cycle
-""",
-)
+#entry(
+#    index = 217,
+#    label = "c7h7s2",
+#    molecule =
+#"""
+#multiplicity 2
+#1  C u0 p0 c0 {2,B} {6,B} {10,S}
+#2  C u0 p0 c0 {1,B} {3,B} {11,S}
+#3  C u0 p0 c0 {2,B} {4,B} {12,S}
+#4  C u0 p0 c0 {3,B} {5,B} {13,S}
+#5  C u0 p0 c0 {4,B} {6,B} {14,S}
+#6  C u0 p0 c0 {1,B} {5,B} {7,S}
+#7  C u0 p0 c0 {6,S} {8,S} {15,S} {16,S}
+#8  S u0 p2 c0 {7,S} {9,S}
+#9  S u1 p2 c0 {8,S}
+#10 H u0 p0 c0 {1,S}
+#11 H u0 p0 c0 {2,S}
+#12 H u0 p0 c0 {3,S}
+#13 H u0 p0 c0 {4,S}
+#14 H u0 p0 c0 {5,S}
+#15 H u0 p0 c0 {7,S}
+#16 H u0 p0 c0 {7,S}
+#""",
+#    thermo = ThermoData(
+#        Tdata = ([300,400,500,600,800,1000,1500],'K'),
+#        Cpdata = ([33.379,41.558,48.886,55.181,64.567,70.718,79.669],'cal/(mol*K)','+|-',[1,1,1,1,1,1,1]),
+#        H298 = (43.829,'kcal/mol','+|-',1),
+#        S298 = (97.491,'cal/(mol*K)','+|-',1),
+#    ),
+#    shortDesc = u"""Ryan Gillis CBS-QB3""",
+#    longDesc =
+#u"""
+#CBS-QB3 calculation for use originally in the DBDS cycle
+#""",
+#)
 
 entry(
     index = 218,
