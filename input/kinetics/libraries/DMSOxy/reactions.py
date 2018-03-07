9647efd70eb6ebe595a2c54569a0275c6d2e6b77
rgillis8 <rgillis@mit.edu>
2018-03-07 12:13:21 -0500

    Added pdep reaction for OH + CSOrad <-> with CSOOH (I think unimportant), added pdep reaction for CSOOH <-> CS(=O)OH (also probably unimportant?), added pdep reaction for CSrad + OOH -> CS(=O)OH (very important?),  pdep reaction of CSrad + O2 -> CS(=O)Orad (unimportant?), corrected units on CSOOrad + CSOOrad <-> CSCOrad + CSCOrad + O2 reaction, Added fast rate for a resonance reaction (CSOrad <-> CSrad=O) (I think unimportant), Added experimental rate for OOrad + CSrad (very important, stop formation for CSOOC and CSOrad in turn),  added an experimental rate for CSrad disprop (important when S=CH2 is allowed back in)

--- input/kinetics/libraries/DMSOxy/reactions.py
+++ input/kinetics/libraries/DMSOxy/reactions.py
@@ -30,7 +30,7 @@ Experimentally observed by Atkinson at 298K
 )
 
 entry(
-    index = 2,
+    index = 3,
     label = "OH + DMSO  <=> DMSO2H",
     degeneracy = 1,
     kinetics = Arrhenius(A=(1.02e+12, 'cm^3/(mol*s)'), n=0, Ea=(0, 'kJ/mol'), T0=(1, 'K')),
@@ -41,18 +41,19 @@ From comparison to the experimentally observed by Atkinson at 298K for OH + DMSO
 )
 
 entry(
-    index = 2,
-    label = "OH + CSOrad  <=> CS(=O)OH",
+    index = 4,
+    label = "CSOOH  <=> OH + CSOrad",
     degeneracy = 1,
-    kinetics = Arrhenius(A=(1.02e+12, 'cm^3/(mol*s)'), n=0, Ea=(0, 'kJ/mol'), T0=(1, 'K')),
+#    kinetics = Arrhenius(A=(5.74e+1, 's^-1'), n=0, Ea=(99.807, 'kJ/mol'), T0=(1, 'K')),
+    kinetics = Arrhenius(A=(5.74e+11, 's^-1'), n=0, Ea=(33.807, 'kJ/mol'), T0=(1, 'K')),
     longDesc =
 u"""
-From comparison to the experimentally observed by Atkinson at 298K for OH + DMSO
+Ryan Gillis pdep calculations
 """,
 )
 
 entry(
-    index = 2,
+    index = 5,
     label = "OH + CSOOH  <=> CS(OH)OOH",
     degeneracy = 1,
     kinetics = Arrhenius(A=(1.02e+12, 'cm^3/(mol*s)'), n=0, Ea=(0, 'kJ/mol'), T0=(1, 'K')),
@@ -63,9 +64,10 @@ From comparison to the experimentally observed by Atkinson at 298K for OH + DMSO
 )
 
 entry(
-    index = 3,
+    index = 6,
     label = "CSOOrad  <=> CS(=O)Orad",
     degeneracy = 1,
+#    kinetics = Arrhenius(A=(4.67589e+10, 's^-1'), n=0.510239, Ea=(0, 'kJ/mol'), T0=(1, 'K')),
     kinetics = Arrhenius(A=(4.67589e+10, 's^-1'), n=0.510239, Ea=(65.5504, 'kJ/mol'), T0=(1, 'K')),
     longDesc =
 u"""
@@ -74,7 +76,40 @@ Calculated at CBS-QB3 by Ryan Gillis
 )
 
 entry(
-    index = 4,
+    index = 7,
+    label = "CSOOH  <=> CS(=O)OH",
+    degeneracy = 1,
+    kinetics = Arrhenius(A=(1.964e+12, 's^-1'), n=0, Ea=(40.945, 'kJ/mol'), T0=(1, 'K')),
+    longDesc =
+u"""
+Calculated at CBS-QB3 with pdep and fitted to Arrhenius parameters for 260K to 310K by Ryan Gillis
+""",
+)
+
+entry(
+    index = 8,
+    label = "CSrad + OOH  <=> CS(=O)OH",
+    degeneracy = 1,
+    kinetics = Arrhenius(A=(1.773e+17, 'cm^3/(mol*s)'), n=0, Ea=(-5.577, 'kJ/mol'), T0=(1, 'K')),
+    longDesc =
+u"""
+Calculated at CBS-QB3 with pdep and fitted to Arrhenius parameters for 260K to 310K by Ryan Gillis
+""",
+)
+
+entry(
+    index = 9,
+    label = "CSrad + O2  <=> CS(=O)Orad",
+    degeneracy = 1,
+    kinetics = Arrhenius(A=(6081212881, 'cm^3/(mol*s)'), n=0, Ea=(35.647, 'kJ/mol'), T0=(1, 'K')),
+    longDesc =
+u"""
+Calculated at CBS-QB3 with pdep and fitted to Arrhenius parameters for 260K to 310K by Ryan Gillis
+""",
+)
+
+entry(
+    index = 10,
     label = "CSCOOrad + DMS  <=> DMSO + CSCOrad",
     degeneracy = 1,
     kinetics = Arrhenius(A=(0.45722, 'cm^3/(mol*s)'), n=3.45093, Ea=(84.1291, 'kJ/mol'), T0=(1, 'K')),
@@ -85,10 +120,10 @@ Calculated at b3lyp/6-31g** by Ryan Gillis
 )
 
 entry(
-    index = 5,
+    index = 11,
     label = "CSCOOrad + CSCOOrad  <=> CSCOrad + CSCOrad + O2",
     degeneracy = 1,
-    kinetics = Arrhenius(A=(7.4e-13, 'cm^3/(mol*s)'), n=0, Ea=(4.324, 'kJ/mol'), T0=(1, 'K')),
+    kinetics = Arrhenius(A=(4.46e+11, 'cm^3/(mol*s)'), n=0, Ea=(4.324, 'kJ/mol'), T0=(1, 'K')),
     longDesc =
 u"""
 by comparison to Atkinson, R.; Baulch, D.L.; Cox, R.A.; Crowley, J.N.; Hampson, R.F, Jr.; Kerr, J.A.; Rossi, M.J.; Troe, J.
@@ -97,7 +132,7 @@ Title: Summary of Evaluated Kinetic and Photochemical Data for Atmospheric Chemi
 )
 
 entry(
-    index = 6,
+    index = 12,
     label = "CSCOOrad + CSCOOrad  <=> CSCOH + MTF + O2",
     degeneracy = 1,
     kinetics = Arrhenius(A=(25, 'cm^3/(mol*s)'), n=0, Ea=(9.728, 'kJ/mol'), T0=(1, 'K')),
@@ -109,15 +144,61 @@ Title: Evaluated kinetic data for combusion modelling. Supplement I reaction for
 )
 
 entry(
-    index = 7,
-    label = "HOOH  <=> OH + OH",
+    index = 13,
+    label = "CSOrad  <=> CSrad=O",
     degeneracy = 1,
-    kinetics = Arrhenius(A=(3.125e-5, 's^-1'), n=0, Ea=(0, 'kJ/mol'), T0=(1, 'K')),
+    kinetics = Arrhenius(A=(1E+10, 's^-1'), n=0, Ea=(0, 'kJ/mol'), T0=(1, 'K')),
     longDesc =
 u"""
-Fitted to the degradation rate.
+Sulfur resonance reaction
 """,
 )
+
+entry(
+    index = 14,
+    label = "CSrad + COOrad  <=> CSOOC",
+    degeneracy = 1,
+    kinetics = Arrhenius(A=(1.8E+06, 'cm^3/(mol*s)'), n=0, Ea=(0, 'kJ/mol'), T0=(1, 'K')),
+    longDesc =
+u"""
+Experimental rate of OO + CSrad at 298. DeMore 1997 JPL Publications.
+""",
+)
+
+entry(
+    index = 15,
+    label = "CSrad + CSCOOrad  <=> S=CH2 + CSCOO",
+    degeneracy = 1,
+    kinetics = Arrhenius(A=(5.91E10, 'cm^3/(mol*s)'), n=2.93, Ea=(102926, 'kJ/mol'), T0=(298, 'K')),
+    longDesc =
+u"""
+Author(s):   Zhu, L.; Bozzelli, J.W.
+Title:   Kinetics of the multichannel reaction of methanethiyl radical (CH3S center dot) with O-3(2) - Pressure Independent
+Journal:   J. Phys. Chem. A
+""",
+)
+
+#entry(
+#    index = 13,
+#    label = "CSCOO  <=> CSCOrad + OH",
+#    degeneracy = 1,
+#   kinetics = Arrhenius(A=(2e+15, 's^-1'), n=0, Ea=(0, 'kJ/mol'), T0=(1, 'K')),
+#    longDesc =
+#u"""
+#Ryan Gillis test
+#""",
+#)
+
+#entry(
+#    index = 10,
+#    label = "HOOH  <=> OH + OH",
+#    degeneracy = 1,
+#    kinetics = Arrhenius(A=(3.125e-0, 's^-1'), n=0, Ea=(0, 'kJ/mol'), T0=(1, 'K')),
+#    longDesc =
+#u"""
+#Fitted to the degradation rate.
+#""",
+#)
 
 #entry(
 #    index = 3,
