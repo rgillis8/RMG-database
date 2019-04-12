ad7e6cf0d04a4e48a7ee5d93d6b6a64361fb6d9d
Ryan Gillis <rgillis@mit.edu>
2019-04-12 13:53:15 -0400

    Additional thermo values for key species

--- input/thermo/libraries/DMSOxy.py
+++ input/thermo/libraries/DMSOxy.py
@@ -122,3 +122,120 @@ u"""
 CBS-QB3 Calculation, Ryan Gillis 5/2017
 """,
 )
+
+entry(
+    index = 1,
+    label = "[O-]O[S+](H)#C",
+    molecule =
+"""
+1 O u0 p3 c-1 {2,S}
+2 O u0 p2 c0 {1,S} {3,S}
+3 S u0 p0 c+1 {2,S} {4,S} {5,T}
+4 H u0 p0 c0 {3,S}
+5 C u0 p0 c0 {3,T} {6,S}
+6 H u0 p0 c0 {5,S}
+""",
+    thermo = ThermoData(
+        Tdata = ([300,400,500,600,800,1000,1500],'K'),
+        Cpdata = ([5,5,5,5,5,5,5],'cal/(mol*K)'),
+        H298 = (50,'kcal/mol'),
+        S298 = (20,'cal/(mol*K)','+|-',0.0007),
+    ),
+    shortDesc = u"""Arbitrary high thermo for an unstable species""",
+    longDesc = 
+u"""
+
+""",
+)
+
+entry(
+    index = 1,
+    label = "[O-]O[S+](=O)=C",
+    molecule =
+"""
+1 O u0 p3 c-1 {2,S}
+2 O u0 p2 c0 {1,S} {3,S}
+3 S u0 p0 c+1 {2,S} {4,D} {5,D}
+4 O u0 p2 c0 {3,D}
+5 C u0 p0 c0 {3,D} {6,S} {7,S}
+6 H u0 p0 c0 {5,S}
+7 H u0 p0 c0 {5,S}
+""",
+    thermo = ThermoData(
+        Tdata = ([300,400,500,600,800,1000,1500],'K'),
+        Cpdata = ([5,5,5,5,5,5,5],'cal/(mol*K)'),
+        H298 = (50,'kcal/mol'),
+        S298 = (20,'cal/(mol*K)','+|-',0.0007),
+    ),
+    shortDesc = u"""Arbitrary high thermo for an unstable species""",
+    longDesc = 
+u"""
+
+""",
+)
+
+entry(
+    index = 1,
+    label = "C=S(=O)=O",
+    molecule =
+"""
+1 C u0 p0 c0 {2,D} {5,S} {6,S}
+2 S u0 p0 c0 {1,D} {3,D} {4,D}
+3 O u0 p2 c0 {2,D}
+4 O u0 p2 c0 {2,D}
+5 H u0 p0 c0 {1,S}
+6 H u0 p0 c0 {1,S}
+""",
+
+    thermo = NASA(
+        polynomials = [
+            NASAPolynomial(
+                coeffs = [3.94543, 0.00311211, 6.05764e-05, -1.15814e-07, 6.56678e-11, -19088.8, 8.49158],
+                Tmin = (10, 'K'),
+                Tmax = (593.934, 'K'),
+            ),
+            NASAPolynomial(
+                coeffs = [3.45685, 0.0194573, -1.36739e-05, 4.5367e-09, -5.67612e-13, -19261, 8.65573],
+                Tmin = (593.934, 'K'),
+                Tmax = (3000, 'K'),
+            ),
+        ],
+        Tmin = (10, 'K'),
+        Tmax = (3000, 'K'),
+    ),
+    shortDesc = u"""""",
+    longDesc =
+u"""
+CBS-QB3 Calculation, Ryan Gillis 5/2017
+""",
+)
+
+entry(
+    index = 1,
+    label = "CSOOrad",
+    molecule =
+"""
+multiplicity 2
+1 C u0 p0 c0 {2,S} {5,S} {6,S} {7,S}
+2 S u0 p2 c0 {1,S} {3,S}
+3 O u0 p2 c0 {2,S} {4,S}
+4 O u1 p2 c0 {3,S}
+5 H u0 p0 c0 {1,S}
+6 H u0 p0 c0 {1,S}
+7 H u0 p0 c0 {1,S}
+""",
+
+    thermo = ThermoData(
+        Tdata = ([300,400,500,600,800,1000,1500],'K'),
+        Cpdata = ([19.595,21.737,23.387,24.748,27.026,28.814,31.606],'cal/(mol*K)'),
+        H298 = (21.388,'kcal/mol'),
+        S298 = (79.357,'cal/(mol*K)','+|-',0.0007),
+    ),
+    shortDesc = u"""""",
+    longDesc =
+u"""
+CBS-QB3 Calculation, Ryan Gillis 5/2017
+""",
+)
+
+
